73c48ba6ce62e6da59d38602c85e2169c2989125
Enio Moreira <eniomoreirasilva@gmail.com>
2022-01-27 16:31:19 -0300

    condicionais que desabilitam o botão de incrementar ou decrementar quando count chegar a um determinado valor (ex.: 0 <= count =< 10).

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,7 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
@@ -9,3 +10,25 @@ function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
+document.getElementById("subtrair").addEventListener("click",function(event){
+    if(currentNumber <= 0){
+        document.getElementById("subtrair").disabled = true;
+        document.getElementById("subtrair").disabled = false;
+    }else{
+        decrement();
+    }
+});
+
+document.getElementById("adicionar").addEventListener("click",function(event){
+    if(currentNumber >= 10){
+        document.getElementById("adicionar").disabled = true;
+        document.getElementById("adicionar").disabled = false;
+    }else{
+        increment();
+    }
+});
+//document.getElementById("subtrair").addEventListener("click", decrement);
+
+//document.getElementById("adicionar").addEventListener("click", increment);
+
